e7771bc7083b2afd0d0a76f1fe22db59c54e8b0c
Alexis La Torre <alexis-latorre@live.fr>
2022-01-05 10:39:49 +0100

    Get processes list and add FA

--- src/components/EADashboard.vue
+++ src/components/EADashboard.vue
@@ -1,20 +1,54 @@
 <template>
-  <button @click="test" type="button" class="btn btn-primary">TEST</button>
+  <div class="m-6">
+    <button @click="getProcesses" type="button" class="btn btn-success">
+      Refresh
+    </button>
+    <table class="table">
+      <thead>
+        <th>Action</th>
+        <th>PID</th>
+        <th>Command</th>
+        <th>Arguments</th>
+        <th>PPID</th>
+      </thead>
+      <tbody v-if="processes && processes.length > 0">
+        <tr v-for="process of processes" :key="process.pid">
+          <td class="align-middle">
+            <button class="btn btn-success">Reload</button>
+          </td>
+          <td class="align-middle">{{ process.pid }}</td>
+          <td class="align-middle">{{ process.command }}</td>
+          <td class="align-middle">{{ process.arguments }}</td>
+          <td class="align-middle">{{ process.ppid }}</td>
+        </tr>
+      </tbody>
+      <tbody v-else>
+        <tr>
+          <th colspan="4">No process found</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      processes: null,
+    };
+  },
   emits: ["alert"],
   methods: {
-    test() {
-      console.log("test");
+    getProcesses() {
       this.axios
         .get("https://wl1th.sse.codesandbox.io/earnapp")
-        .then((res) => {
-          console.log(res);
-        })
+        .then((res) => (this.processes = res.data))
         .catch((e) => this.$emit("alert", e.message));
     },
+  },
+  mounted() {
+    this.getProcesses();
   },
 };
 </script>
