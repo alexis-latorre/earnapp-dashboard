cdb49cf05aa4e55e574edcd8b64e277570229e77
Alexis La Torre <alexis-latorre@live.fr>
2022-01-05 09:16:11 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="test">TEST</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    test() {
+      console.log("test");
+      this.axios.get("https://wl1th.sse.codesandbox.io/").then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
