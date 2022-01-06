fac421ade7dd49a1248ca65a30bc126b2e20a291
Alexis La Torre <alexis-latorre@live.fr>
2022-01-06 09:41:36 +0100

    Refactor methods names

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="container">
-    <EADashboard v-on:alert="alert" />
-  </div>
-  <div
-    v-if="displayAlert"
-    @click="displayAlert = false"
-    :class="`alert alert-${alertColor}`"
-    role="alert"
-  >
-    {{ alertMessage }}
+  <div class="app">
+    <div class="header"></div>
+    <div class="content">
+      <div class="container">
+        <EADashboard v-on:alert="alert" />
+      </div>
+      <div class="container">
+        <div
+          v-if="displayAlert"
+          @click="displayAlert = false"
+          :class="`alert alert-${alertColor}`"
+          role="alert"
+        >
+          {{ alertMessage }}
+        </div>
+      </div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -44,7 +52,34 @@ export default {
 
       this.alertMessage = msg;
       this.displayAlert = true;
+      setTimeout(() => {
+        this.displayAlert = false;
+      }, 5000);
     },
   },
 };
 </script>
+
+<style lang="scss">
+.app {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 1fr 80% 1fr;
+  grid-template-rows: 50px 1fr auto;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.header {
+  grid-area: 1 / 1 / 2 / 4;
+}
+.content {
+  grid-area: 2 / 1 / 3 / 4;
+}
+.footer {
+  grid-area: 3 / 1 / 4 / 4;
+}
+.small {
+  font-size: 0.6em;
+}
+</style>
--- src/components/EADashboard.vue
+++ src/components/EADashboard.vue
@@ -1,64 +1,199 @@
 <template>
-  <div class="m-6">
+  <div class="mt-2">
     <button
       v-if="canRefresh"
-      @click="getProcesses"
+      @click="getServers"
       type="button"
-      class="btn btn-success"
+      class="btn btn-success btn-sm"
     >
       <i class="fas fa-sync-alt"></i>
     </button>
-    <button v-else type="button" class="btn btn-success" disabled>
+    <button v-else type="button" class="btn btn-success btn-sm" disabled>
       <i class="fas fa-spin fa-sync-alt"></i>
     </button>
-    <table class="table">
+    <table class="table table-bordered mt-4">
       <thead>
-        <th>Action</th>
-        <th>PID</th>
-        <th>Command</th>
-        <th>Arguments</th>
-        <th>PPID</th>
+        <tr>
+          <th>Host</th>
+          <th>Action</th>
+          <th>PID</th>
+          <th>Command</th>
+          <th>Arguments</th>
+          <th>PPID</th>
+        </tr>
       </thead>
-      <tbody v-if="processes && processes.length > 0">
-        <tr v-for="process of processes" :key="process.pid">
+      <tbody v-if="servers && servers.length > 0">
+        <tr
+          v-for="server of servers"
+          :key="server.pid"
+          :class="server.status === 'up' ? 'table-success' : 'table-danger'"
+        >
           <td class="align-middle">
-            <button class="btn btn-success">Reload</button>
+            {{ server.host }}<br />
+            <span class="small">{{ server.address }}</span>
           </td>
-          <td class="align-middle">{{ process.pid }}</td>
-          <td class="align-middle">{{ process.command }}</td>
-          <td class="align-middle">{{ process.arguments }}</td>
-          <td class="align-middle">{{ process.ppid }}</td>
+          <td class="align-middle">
+            <template v-if="!server.reachable">
+              <span class="text-danger"
+                ><i class="fas fa-satellite-dish"></i> Offline</span
+              >
+            </template>
+            <template v-else>
+              <template v-if="server.status === 'down'">
+                <button
+                  v-if="server.starting"
+                  class="btn btn-success btn-sm"
+                  disabled
+                >
+                  <i class="fas fa-play"></i>
+                </button>
+                <button
+                  v-else
+                  class="btn btn-success btn-sm"
+                  @click="startServer(server)"
+                >
+                  <i class="fas fa-play"></i>
+                </button>
+              </template>
+              <template v-else>
+                <template v-if="server.stopping">
+                  <button class="btn btn-warning btn-sm" disabled>
+                    <i
+                      v-if="server.restarting"
+                      class="fas fa-spin fa-sync-alt"
+                    ></i>
+                    <i v-else class="fas fa-sync-alt"></i>
+                  </button>
+                  <button class="btn btn-danger btn-sm mx-2" disabled>
+                    <i class="fas fa-stop"></i>
+                  </button>
+                </template>
+                <template v-else>
+                  <button
+                    class="btn btn-warning btn-sm"
+                    @click="restartServer(server)"
+                  >
+                    <i class="fas fa-sync-alt"></i>
+                  </button>
+                  <button
+                    class="btn btn-danger btn-sm mx-2"
+                    @click="stopServer(server)"
+                  >
+                    <i class="fas fa-stop"></i>
+                  </button>
+                </template>
+              </template>
+            </template>
+          </td>
+          <td class="align-middle">{{ server.pid }}</td>
+          <td class="align-middle">{{ server.command }}</td>
+          <td class="align-middle">{{ server.arguments }}</td>
+          <td class="align-middle">{{ server.ppid }}</td>
         </tr>
       </tbody>
       <tbody v-else>
-        <tr>
-          <th colspan="4">No process found</th>
+        <tr v-if="canRefresh">
+          <td colspan="6">No host found</td>
+        </tr>
+        <tr v-else>
+          <td colspan="6">Fetching data...</td>
         </tr>
       </tbody>
     </table>
   </div>
+  <button @click="test">test</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      processes: null,
+      servers: null,
       canRefresh: true,
     };
   },
   emits: ["alert"],
   methods: {
-    getProcesses() {
+    test() {
+      this.axios
+        .get("https://82.64.246.23")
+        .then((res) => console.log(res))
+        .catch((e) => {
+          this.$emit("alert", e.message);
+          this.canRefresh = true;
+        });
+    },
+    getServers() {
       if (!this.canRefresh) return;
 
-      this.processes = null;
+      this.servers = null;
       this.canRefresh = false;
       this.axios
         .get("https://wl1th.sse.codesandbox.io/earnapp")
         .then((res) => {
-          this.processes = res.data;
+          this.servers = res.data;
           this.canRefresh = true;
+        })
+        .catch((e) => {
+          this.$emit("alert", e.message);
+          this.canRefresh = true;
+        });
+    },
+    startServer(server) {
+      server.starting = true;
+      server.restarting = false;
+      this.axios
+        .post("https://wl1th.sse.codesandbox.io/earnapp/start/" + server.id)
+        .then((res) => {
+          this.servers = res.data;
+          this.canRefresh = true;
+          server.starting = false;
+        })
+        .catch((e) => {
+          this.$emit("alert", e.message);
+          this.canRefresh = true;
+          server.starting = false;
+        });
+    },
+    stopServer(server) {
+      server.stopping = true;
+      server.restarting = false;
+      this.axios
+        .post("https://wl1th.sse.codesandbox.io/earnapp/stop/" + server.id)
+        .then((res) => {
+          this.servers = res.data;
+          this.canRefresh = true;
+          server.stopping = false;
+        })
+        .catch((e) => {
+          this.$emit("alert", e.message);
+          this.canRefresh = true;
+          server.stopping = false;
+        });
+    },
+    restartServer(server) {
+      server.stopping = true;
+      server.starting = true;
+      server.restarting = true;
+      this.axios
+        .post("https://wl1th.sse.codesandbox.io/earnapp/stop/" + server.id)
+        .then((_) => {
+          this.axios
+            .post("https://wl1th.sse.codesandbox.io/earnapp/start/" + server.id)
+            .then((res) => {
+              this.servers = res.data;
+              this.canRefresh = true;
+              server.starting = false;
+              server.stopping = false;
+              server.restarting = false;
+            })
+            .catch((e) => {
+              this.$emit("alert", e.message);
+              this.canRefresh = true;
+              server.starting = false;
+              server.stopping = false;
+              server.restarting = false;
+            });
         })
         .catch((e) => {
           this.$emit("alert", e.message);
@@ -67,7 +202,7 @@ export default {
     },
   },
   mounted() {
-    this.getProcesses();
+    this.getServers();
   },
 };
 </script>
